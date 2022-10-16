75dab2295079fde1597cc187f3bc1bca8cb47ccf
Joonbeom <jbahn@mobigen.com>
2022-10-16 10:07:26 +0900

    Initial commit

---
+++ src/main/java/item2/Pizza.java
@@ -0,0 +1,14 @@
+package item2;
+
+import java.util.Set;
+
+public abstract class Pizza {
+    public enum Topping {
+        HAM,
+        MUSHROOM,
+        ONION,
+        PEPPER,
+        SAUSAGE
+    }
+    final Set<Topping> toppings;
+}
